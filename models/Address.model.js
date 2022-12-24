2c94ed97e029a30a35fd8e4a9e218a0d383b4bdc
David Lazaro <dlazaro7@gmail.com>
2022-12-24 11:40:28 +0100

    Address.model.js added

---
+++ models/Address.model.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+const addressSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+    street: {
+      type: String,
+      trim: true,
+      required: [true, "Street is required"],
+    },
+    number: {
+      type: String,
+      required: [true, "Number is required"],
+      trim: true,
+    },
+    cp: {
+      type: Number,
+      required: [true, "cp is required"],
+      trim: true,
+    },
+    town: {
+      type: String,
+      required: [true, "City is required"],
+      trim: true,
+    },
+    province: {
+      type: String,
+      required: [true, "Province is required"],
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: [true, "Country is required"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Address = model("Address", addressSchema);
+module.exports = Address;
